0ef0006a1bbadbe12f2af4565e3d2db786c26ccd
Fredrick Idemudia <mymailserver888@gmail.com>
2021-12-21 21:07:17 +0100

    Module 6 number 1 to 9 Practice

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
+import {Product} from './product'
 
 
 @Injectable()  
 export class ProductService {
 
     private _albumUrl = '../assets/album.json';
+    private _productUrl = '../assets/products.json'
 
   constructor( private _http: Http ) { }
 
